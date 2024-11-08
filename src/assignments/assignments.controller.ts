b0cfc13b7cf1228443bbf1ec58875c498845294e
KaitoKid03 <bvpbull.26@gmail.com>
2024-11-08 20:29:36 +0800

    Completed Assignment: Prime Checker

---
+++ src/assignments/assignments.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('assignments')
+export class AssignmentsController {
+
+  @Get('prime/:number')
+  checkPrime(@Param('number') number: string) {
+    const num = parseInt(number, 10);
+
+    // Check if the number is prime
+    const isPrime = this.isPrime(num);
+
+    return { isPrime };
+  }
+
+  private isPrime(num: number): boolean {
+    if (num <= 1) return false;
+    if (num === 2 || num === 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+    for (let i = 5; i * i <= num; i += 6) {
+      if (num % i === 0 || num % (i + 2) === 0) return false;
+    }
+    return true;
+  }
+}
